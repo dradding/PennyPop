a38af7d140c5b989c2f6d03db6a53ef55ec8bbbe
Daniel Radding <dradding@gmail.com>
2014-04-17 19:43:30 -0700

    Left side graphics

--- src/com/pennypop/project/MainScreen.java
+++ src/com/pennypop/project/MainScreen.java
@@ -5,7 +5,11 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 
 /**
  * This is where you screen code will go, any UI should be in here
@@ -17,12 +21,21 @@ public class MainScreen implements Screen {
 	private final Stage stage;
 	private final SpriteBatch spriteBatch;
 	private final BitmapFont font;
+	private final TextureAtlas buttons; //Image of all buttons
+	private final TextureRegion SFX;
+	private final TextureRegion API;
+	private final TextureRegion Game;
+	
 	
 	public MainScreen() {
 		spriteBatch = new SpriteBatch();
 		stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, spriteBatch);
 		font = new BitmapFont(Gdx.files.internal("assets/font.fnt"),
 		         Gdx.files.internal("assets/font.png"), false);
+		buttons = new TextureAtlas("assets/PPButtons.pack");
+		SFX = buttons.findRegion("sfxButton");
+		API = buttons.findRegion("apiButton");
+		Game = buttons.findRegion("gameButton");
 		
 	}
 
@@ -38,8 +51,11 @@ public class MainScreen implements Screen {
 		stage.draw();
 		spriteBatch.begin();
 		font.setColor(Color.RED);
-		font.draw(spriteBatch, "PennyPop", 640 - font.getBounds("PennpyPop").width/2, 
-				360 + font.getBounds("PennyPop").height/2);
+		font.draw(spriteBatch, "PennyPop", 455 - font.getBounds("PennpyPop").width/2, 
+				460 + font.getBounds("PennyPop").height/2);
+		spriteBatch.draw(SFX, 295, 310);
+		spriteBatch.draw(API, 405, 310);
+		spriteBatch.draw(Game, 515, 310);
 		spriteBatch.end();
 	}
 
